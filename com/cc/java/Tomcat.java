feac4cf066ed0e7d4e1aa86de6bd1f8b7a124cf7
Ozan Oezyurt <ozan.oezyurt@icloud.com>
2023-05-25 10:45:19 +0200

    SUPERCAT!

--- com/cc/java/Tomcat.java
+++ com/cc/java/Tomcat.java
@@ -1,34 +1,24 @@
 package com.cc.java;
 
-public class Tomcat {
-
-  private String name;
-  private String furColor;
-  private int age; 
+public class Tomcat extends Supercat{
 
 
 
 
-  public Tomcat(String name, String furColor, int age) {
-    this.name = name;
-    this.furColor = furColor;
-    this.age = age;
-}
 
-public String getStringAttributes(String op) {
-    switch (op) {
-      case "#name":
-        return name;
-      case "#color":
-        return furColor;
-      default:
-        return "ERROR!";
-    }
+
+  public Tomcat (String name, String furColor, int age) {
+    super(name, furColor, age);
+
   }
+
 
   public String getAge() {
+    
     return String.valueOf(age);
+    
   }
+
 
 }
   
